765858ab815713355a516cd5d0b35cfd6835080b
anvarrexx <86181239+anvarrexx@users.noreply.github.com>
2021-07-31 12:08:19 +0500

    добавил урок 26 многостраничник

---
+++ lesson_26/nav.php
@@ -0,0 +1,26 @@
+<!-- Top menu -->
+		<nav class="navbar navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.php">Lancar - Bootstrap One-Page Portfolio Template</a>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="top-navbar-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="what-we-do.php">Services</a></li>
+						<li><a href="clients.php">Clients</a></li>
+						<li><a href="portfolio.php">Portfolio</a></li>
+						<li><a href="price.php">Prices</a></li>
+						<li><a href="about-us.php">About</a></li>
+						<li><a href="contact-block.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
